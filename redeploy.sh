577062a5d4f3b7a2aed52bb3c3854c89984a3123
philios33 <phil@code67.com>
2021-12-20 21:14:45 +0100

    Redeploy quit on fail

--- redeploy.sh
+++ redeploy.sh
@@ -3,9 +3,15 @@
 cd "$(dirname "$0")"
 
 git pull
+if [ $? -ne 0 ]; then { exit $?; } fi
 
 npm run downloadCertBundle
+if [ $? -ne 0 ]; then { exit $?; } fi
+
 npm run downloadFlags
+if [ $? -ne 0 ]; then { exit $?; } fi
 
 docker-compose build
+if [ $? -ne 0 ]; then { exit $?; } fi
+
 docker-compose up -d --force-recreate
